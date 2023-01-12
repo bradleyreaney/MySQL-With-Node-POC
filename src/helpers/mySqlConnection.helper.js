ae1ed49e32e55876afb3b0918b260394a1341b5f
Brad <bradley.reaney@nimbleapproach.com>
2023-01-12 11:38:27 +0000

    Fixing linter issues

--- src/helpers/mySqlConnection.helper.js
+++ src/helpers/mySqlConnection.helper.js
@@ -1,27 +1,27 @@
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 import mysql from 'mysql';
 
-dotenv.config()
+dotenv.config();
 
 // Config for the DB connection
 const mysqlConfig = {
-	host: process.env.MYSQL_HOST,
-	port: process.env.MYSQL_PORT,
-	user: process.env.MYSQL_USERNAME,
-	password: process.env.MYSQL_PASSWORD,
-	connectionLimit: 10
-}
+    host: process.env.MYSQL_HOST,
+    port: process.env.MYSQL_PORT,
+    user: process.env.MYSQL_USERNAME,
+    password: process.env.MYSQL_PASSWORD,
+    connectionLimit: 10
+};
 
 var con = mysql.createConnection(mysqlConfig);
 
 // Creates the connection to the DB
-con.connect(function (err) {
-	if (err) throw err;
-	console.log("Connected to MySQL Server!");
+con.connect(function(err) {
+    if (err) throw err;
+    console.log('Connected to MySQL Server!');
 });
 
 // Closes the connection to the DB
-con.end(function (err) {
-	if (err) throw err;
-	console.log("Closed connected to MySQL Server!");
+con.end(function(err) {
+    if (err) throw err;
+    console.log('Closed connected to MySQL Server!');
 });
--- src/helpers/mySqlDataSetup.helper.js
+++ src/helpers/mySqlDataSetup.helper.js
@@ -1,7 +1,7 @@
-import * as dotenv from 'dotenv'
+import * as dotenv from 'dotenv';
 import mysql from 'mysql';
 
-dotenv.config()
+dotenv.config();
 
 // Config for the DB connection
 const mysqlConfig = {
@@ -10,7 +10,7 @@ const mysqlConfig = {
     user: process.env.MYSQL_USERNAME,
     password: process.env.MYSQL_PASSWORD,
     connectionLimit: 10
-}
+};
 
 const dbName = 'mydb';
 const tableName = 'customerData';
@@ -18,7 +18,7 @@ const tableName = 'customerData';
 var con = mysql.createConnection(mysqlConfig);
 
 // Creates the connection to the DB
-con.connect(function (err) {
+con.connect(function(err) {
     if (err) throw err;
     console.log('Connected to MySQL Server');
     console.log(`Checking for ${dbName}`);
@@ -26,8 +26,8 @@ con.connect(function (err) {
     // Checks if the DB exists
     if (!con.query(`USE ${dbName}`)) {
         // If it doesn't then it will start the setup and create it
-        console.log('Database did not exist. Creating DB.')
-        con.query(`CREATE DATABASE ${dbName}`, function (err) {
+        console.log('Database did not exist. Creating DB.');
+        con.query(`CREATE DATABASE ${dbName}`, function(err) {
             if (err) throw err;
         });
         console.log('Database created');
@@ -35,46 +35,46 @@ con.connect(function (err) {
         // If it does then it will drop it and recreate the data
 
         // Dropping database
-        console.log('Database did exist. Dropping DB')
-        con.query(`DROP DATABASE ${dbName}`, function (err) {
+        console.log('Database did exist. Dropping DB');
+        con.query(`DROP DATABASE ${dbName}`, function(err) {
             if (err) throw err;
         });
 
         // Creating databse
-        console.log('Creating the new Database')
-        con.query(`CREATE DATABASE ${dbName}`, function (err) {
+        console.log('Creating the new Database');
+        con.query(`CREATE DATABASE ${dbName}`, function(err) {
             if (err) throw err;
         });
-        con.query(`USE ${dbName}`, function (err) {
+        con.query(`USE ${dbName}`, function(err) {
             if (err) throw err;
             console.log('Database created');
         });
 
         // Creating table
         console.log('Creating the table');
-        var createTable = `CREATE TABLE ${tableName} (id VARCHAR(255), panId VARCHAR(255), name VARCHAR(255), pan VARCHAR(255))`
-        con.query(createTable, function (err) {
+        var createTable = `CREATE TABLE ${tableName} (id VARCHAR(255), panId VARCHAR(255), name VARCHAR(255), pan VARCHAR(255))`;
+        con.query(createTable, function(err) {
             if (err) throw err;
             console.log('Table created');
         });
 
         // Inserting the data
         console.log('Insert customer data');
-        var insertCustomerData = `INSERT INTO ${tableName} (id, panId, name, pan) VALUES ('1', '01', 'Test One', '11112222333344445555')`
-        con.query(insertCustomerData, function (err) {
+        var insertCustomerData = `INSERT INTO ${tableName} (id, panId, name, pan) VALUES ('1', '01', 'Test One', '11112222333344445555')`;
+        con.query(insertCustomerData, function(err) {
             if (err) throw err;
             console.log('Customer data inserted');
         });
 
         // Return customer data
         var selectCustomerData = `SELECT * FROM ${tableName}`;
-        con.query(selectCustomerData, function (err, result) {
+        con.query(selectCustomerData, function(err, result) {
             if (err) throw err;
             console.table(result);
-        })
+        });
 
         // Closes the connection
-        con.end(function (err) {
+        con.end(function(err) {
             if (err) throw err;
             console.log('Closed connected to MySQL Server');
         });
